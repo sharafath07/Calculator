7276fc306de503fab2a8897ba29b92235fb8cfd1
sharafath07 <142240872+sharafath07@users.noreply.github.com>
2024-06-19 12:04:42 +0000

    hello

--- script.js
+++ script.js
@@ -1,49 +1,63 @@
-        let calculation = JSON.parse(localStorage.getItem('calculation')) || '';
-        let result = 0
+let calculation = JSON.parse(localStorage.getItem('calculation')) || '0';
+let result = 0
+let arrayValue = []
 
-        console.log(calculation)
+function number(value) {
+    const num = '' + value
+    arrayValue.push(num);
+    if(calculation !== '0'){
+        calculation += num;
+    }else{
+        calculation = '';
+        calculation += num;
+    }
+    document.querySelector('.calculation').innerHTML = `${calculation}`
 
-        function number(value) {
-            calculation += value;
-            document.querySelector('.calculation').innerHTML = `${calculation}`
+    equal()
+}
+
+function operator(value) {
+    const num = '' + value
+    arrayValue.push(num);
+    if(calculation !== '0'){        
+        calculation += num;
+    }else{
+        return 0;
+    }
+    document.querySelector('.calculation').innerHTML = `${calculation}`
+}
+
+function equal() { 
+    result = eval(calculation);  /*eval() converts the calculation into actual math*/
+    document.querySelector('.result').innerHTML = `${result}`
+    localStorage.setItem('calculation',JSON.stringify(calculation))
+}
+
+function remove() {
+    calculation = '0'
+    result = '0'
+    arrayValue = []
+    localStorage.removeItem('calculation')
+    document.querySelector('.calculation').innerHTML = `${calculation}`
+    document.querySelector('.result').innerHTML = `${result}`
+}
+
+function del(){
+    let space = ''
+    let num = arrayValue.length - 1
+    arrayValue.splice(num, 1)
+
+    if (num <= 0) {
+        space = '0' 
+    } else {
+        for (let i = 0; i < arrayValue.length; i++) {
+            const element = arrayValue[i];
+            space += element
+            
         }
+    }
 
-        function operator(value) {
-            calculation += value;
-            document.querySelector('.calculation').innerHTML = `${calculation}`
-        }
+    calculation = space
 
-        function add() {
-            calculation += ' + ';
-            document.querySelector('.calculation').innerHTML = `${calculation}`
-        }
-
-        function substract() {
-            calculation += ' - ';
-            document.querySelector('.calculation').innerHTML = `${calculation}`
-        }
-
-        function multiple() {
-            calculation += ' * ';
-            document.querySelector('.calculation').innerHTML = `${calculation}`
-        }
-
-        function divide() {
-            calculation += ' / ';
-            document.querySelector('.calculation').innerHTML = `${calculation}`
-        }
-
-        function equal() { 
-            result = eval(calculation);  /*eval() converts the calculation into actual math*/
-            document.querySelector('.result').innerHTML = `${result}`
-            localStorage.setItem('calculation',JSON.stringify(calculation))
-        }
-
-        function remove() {
-            calculation = ' '
-            result = '0'
-            localStorage.removeItem('calculation')
-            document.querySelector('.calculation').innerHTML = `${calculation}`
-            document.querySelector('.result').innerHTML = `${result}`
-
-        }
+    document.querySelector('.calculation').innerHTML = `${calculation}`
+}
